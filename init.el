17cf08eb148f00c82f7f3150879c4b2963933617
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-09-03 22:14:36 +0900

    feat: use c-h as backspace

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; Display line numbers in the left fringe
 (global-display-line-numbers-mode t)
+
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
 ;; Load a built-in theme
 (load-theme 'tango-dark t)
 
