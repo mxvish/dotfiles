df86ebb6de98b17b4b236264fee15b515cb36180
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-12-13 12:50:39 +0900

    feat: check keyboard conf

--- check_diff.sh
+++ check_diff.sh
@@ -9,5 +9,6 @@ check_diff(){
 
 if [ "$(uname)" = "Linux" ]; then
   check_diff ".vimrc" "/home/$HOSTNAME/"
+  check_diff "00-keyboard.conf" "/etc/X11/xorg.conf.d/"
   check_diff "init.el" "/home/$HOSTNAME/.emacs.d/"
 fi
