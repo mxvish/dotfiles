a2464def2fbe781bacca56117de3714f7499b6fd
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-08-16 15:36:40 +0900

    feat: check fcitx5

--- check_diff.sh
+++ check_diff.sh
@@ -11,4 +11,6 @@ if [ "$(uname)" = "Linux" ]; then
   check_diff ".vimrc" "/home/$HOSTNAME/"
   check_diff "00-keyboard.conf" "/etc/X11/xorg.conf.d/"
   check_diff "init.el" "/home/$HOSTNAME/.emacs.d/"
+  check_diff "config" "/home/$HOSTNAME/.config/fcitx5"
+  check_diff "profile" "/home/$HOSTNAME/.config/fcitx5"
 fi
