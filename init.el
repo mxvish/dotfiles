a0990c85e252ba9091718fd39abde94b4e6ce77c
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-08-29 13:26:57 +0900

    feat: setup company

--- init.el
+++ init.el
@@ -16,3 +16,15 @@
 
 (with-eval-after-load 'company
   (define-key company-active-map (kbd "C-h") #'delete-backward-char))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
