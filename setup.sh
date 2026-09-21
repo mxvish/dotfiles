26e165f83c94e33132752501d39bf7ef0462fcbf
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-09-21 21:49:17 +0900

    feat: configure nvim instead of vim

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-mv .vimrc /home/$(hostname)/.vimrc
+#mv .vimrc /home/$(hostname)/.vimrc
+
+mkdir -p /home/$(hostname)/.config/nvim
+mv .vimrc /home/$(hostname)/.config/nvim/init.vim
 
 mkdir -p /home/$(hostname)/.emacs.d/
 mv init.el /home/$(hostname)/.emacs.d/
