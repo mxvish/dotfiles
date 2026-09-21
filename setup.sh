739eb08680af087d3c2f79b1b1ee00840f2059eb
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-09-21 21:47:43 +0900

    feat: configure dotfiles using hostname

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mv .vimrc ~/.vimrc
+mv .vimrc /home/$(hostname)/.vimrc
 
-mkdir -p ~/.emacs.d/
-mv init.el ~/.emacs.d/
+mkdir -p /home/$(hostname)/.emacs.d/
+mv init.el /home/$(hostname)/.emacs.d/
 
-mv config ~/.config/fcitx5
-mv profile ~/.config/fcitx5
+mv config /home/$(hostname)/.config/fcitx5
+mv profile /home/$(hostname)/.config/fcitx5
